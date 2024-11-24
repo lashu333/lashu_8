c3e9c8a901f1472974d0bb92c5fa838aac844e47
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-11-24 19:42:23 +0400

    Initial Commit

---
+++ lashu_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lashu_8
+//
+//  Created by Lasha Tavberidze on 24.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
